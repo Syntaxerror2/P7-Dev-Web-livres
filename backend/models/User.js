8cb7d02a92c6eb5ba7df8a0c51b42fd00f3954b7
Antoine <nonown745@gmail.com>
2026-02-11 19:52:38 +0100

    Ajout controller User + uniqueValidator

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+//Ajout du package permettant d'imposer une adresse mail unique
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 })
+
+//Méthode plugin permettant d'appliquer uniqueValidator
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
