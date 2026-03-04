2ed5e4207e248831cc881714780e70e532f576bc
Antoine <nonown745@gmail.com>
2026-03-04 18:52:23 +0100

    Finale commit

--- backend/app.js
+++ backend/app.js
@@ -13,18 +13,17 @@ const userRoute = require('./routes/user')
 
 
 //Travail effectué depuis une BDD en dur, faute d'autre possibilité
-
-
+//Je commente et laisse le lien de la BDD exportée en ligne pour évaluation
+/* 
 mongoose.connect('mongodb://127.0.0.1:27017/livres')
   .then(() => console.log('Connexion MongoDB locale réussie !'))
   .catch(err => console.error(err));
+*/
 
-/* 
-//Je commente celle-ci et ajoute ci-dessous l'URL de la BDD exportée en ligne
 mongoose.connect('mongodb+srv://syntaxerror:1234@cluster0.28mlqmf.mongodb.net/?appName=Cluster0')
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(err => console.error(err));
-*/
+
 
 
 const app = express();
